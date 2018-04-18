2ee4fc2ad8f2e7c0a83efefffaf540413e7dd2c3
Robin Vinck <robin.vinck@student.ehb.be>
2018-04-18 14:58:54 +0200

    Initial Commit

---
+++ Robin_Vinck_S2IT_werkstuk1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Robin_Vinck_S2IT_werkstuk1
+//
+//  Created by Robin Vinck on 18/04/18.
+//  Copyright © 2018 Robin Vinck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
